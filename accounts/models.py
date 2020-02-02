d2f4f7d52d12ccab94877bf2670c72ed67eaf8f3
Mohit Paudel <39890575+MohitP197@users.noreply.github.com>
2020-02-02 08:51:46 +0545

    Created product and order models and added status for choosing category from choices

--- accounts/models.py
+++ accounts/models.py
@@ -9,7 +9,32 @@ class Customer(models.Model):
     email = models.CharField(max_length=200, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
 
-
     # For showing customer name instead of the customer object in admins dashboard
+
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    CATEGORY = (
+        ('Indoor', 'Indoor'),
+        ('Out Door', 'Out Door'),
+    )
+
+    name = models.CharField(max_length=200, null=True)
+    price = models.FloatField(null=True)
+    category = models.CharField(max_length=200, null=True, choices=CATEGORY)
+    description = models.CharField(max_length=200, null=True)
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+
+
+class Order(models.Model):
+    STATUS = (
+        ('Pending', 'Pending'),
+        ('Out for delivery', 'Out for delivery'),
+        ('Delivered', 'Delivered'),
+    )
+    # customer =
+    # product =
+    date_created = models.DateTimeField(auto_now_add=True, null=True)
+    status = models.CharField(max_length=200, null=True, choices=STATUS)
